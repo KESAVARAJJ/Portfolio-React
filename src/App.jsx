f6b78439363aa74f57d0dc91b9e6da99d3eefd79
KESAVARAJJ <kesavarajj04@gmail.com>
2024-10-07 18:23:22 +0530

    changes done for calling the function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './index.css'
 import './App.css'
+import Hero from './Pages/Hero.jsx'
+import About from './Pages/About.jsx'
+import Project from './Pages/Project.jsx'
+import Resume from './Pages/Resume.jsx'
+import Contact from './Pages/Contact.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Hero/>
+    <About/>
+    <Project/>
+    <Resume/>
+    <Contact/>
 
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -4,23 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import './App.css'
 import Header from './Components/Header.jsx'
-import Hero from './Pages/Hero.jsx'
-import About from './Pages/About.jsx'
-import Project from './Pages/Project.jsx'
-import Resume from './Pages/Resume.jsx'
-import Contact from './Pages/Contact.jsx'
 import Footer from './Components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
-    <Hero/>
-    <About/>
-    <Project/>
-    <Resume/>
-    <Contact/>
-    <Footer/>
-  
     <App />
+    <Footer/>
   </StrictMode>,
 )
